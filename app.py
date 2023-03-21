7655157694432cd8cfc2eb22d6bb05c30034fb16
XausMaster <ibrahimisazade264@gmail.com>
2023-03-21 22:13:25 +0400

    V 0.1

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello World !'
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return 'About'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return f'User info: {name} , {id}'
 
 
 if __name__ == '__main__':
